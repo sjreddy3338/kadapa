a076850e7f07eb18f77cafe8935d20384890455c
sjreddy3338 <sjreddy3338@gmail.com>
2019-12-06 10:29:55 +0530

    today

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, Kadapa");
-    System.out.Println("Hello, Bangore");
+    System.out.println("Hello, Bangore");
     System.out.println("Hello, Hydrabad");
   }
 }
